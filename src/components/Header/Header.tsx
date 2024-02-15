f3becbe3aa1439aae4fbb7f29ca74caf50effcdb
Fallougin <artursuvor@mail.ru>
2024-02-15 17:30:26 +0100

    v1

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isSticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+        setSticky(window.scrollY > 110);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    };
+}, []);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
   return (
-    <div className="header">
-      <div className="Main-logo">
-        <img src="./img/logo.svg" alt="Main-Logo" />
-      </div>
-      <div className="menu-controls">
-        <span className="phone-number">
-            <img src="./img/phone.svg" alt="phone" className='phone-icon' />
-            <a href="tel:3826977200">382 69 772-002</a>
-        </span>
-        <button className="menu-button" onClick={toggleMenu}>
-            <svg className="menu-icon" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M5 9H27M5 16H16M5 23H27"
-                    stroke="#FFFFFF"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                />
-            </svg>
-        </button>
-      </div>
+    <div>
       {menuOpen && (
         <div className="menu">
           <ul>
@@ -49,8 +41,37 @@ function Header() {
               <a href="#">Контакты</a>
             </li>
           </ul>
+          <div className="additional-content">
+            <a href="#">Социальная ответственность</a>
+            <a href="#">Гарантии</a>
+            <a href="#">FAQ</a>
+            <a href="#">Сотрудничество</a>
+          </div>
         </div>
       )}
+
+      <div className={isSticky ? 'sticky-header' : 'header'}>
+        <div className="main-logo">
+          <img src="./img/logo.svg" alt="logo-on-main-page" />
+        </div>
+        <div className={isSticky ? "sticky-menu-controls" : "menu-controls"}>
+          <span className={isSticky ? "sticky-phone-number" : "phone-number"}>
+            <img src= {isSticky ? "./img/black-phone.svg" : "./img/phone.svg" }alt="phone" className='phone-icon' />
+            <a href="tel:3826977200">382 69 772-002</a>
+          </span>
+          <button className={isSticky ? "sticky-menu-button" : 'menu-button' } onClick={toggleMenu}>
+            <svg className="menu-icon" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M5 9H27M5 16H16M5 23H27"
+                stroke={isSticky ? "black" : "#FFFFFF"}
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,31 +1,77 @@
 import React, { useState, useEffect } from 'react';
+import './Main.css'; 
 
 function Main() {
   const [showHero, setShowHero] = useState(true);
+  const [language, setLanguage] = useState('ru'); // Initial language is Russian
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setShowHero(false);
-    }, 10);
+    }, 250);
 
     return () => clearTimeout(timeout);
   }, []);
 
+  // Function to toggle between Russian and English
+  const toggleLanguage = () => {
+    setLanguage(language === 'ru' ? 'en' : 'ru');
+  };
+
   return (
     <div>
-      {showHero && (
-        <div className="hero">
-          <div className="hero-content">
-            <h1>MÁRO</h1>
-            <h2>Design Studio</h2>
+      <div className={`hero ${showHero ? '' : 'fade-out'}`}>
+        <div className="hero-content">
+          <img src="./img/logo.svg" alt="Main-Logo" />
+        </div>
+      </div>
+      <div className={`content ${showHero ? 'content-faded' : ''}`}>
+        <div className="left-side">
+          <div className="centered-content">
+            <img src="./img/main-img-1.jpg" alt="left-side-img" />
+          </div>
+          <div className="text-content">
+            <p className='interior-text'>ИНТЕРЬЕРЫ ДЛЯ ЖИЗНИ В ЧЕРНОГОРИИ</p>
+            <p className='interior-description'>Создаем внутренние пространства квартир и вилл. 
+              Подбор и доставка мебели из Европы</p>
+            <button className='calculate-btn'>Рассчитать стоимость</button>
           </div>
         </div>
-      )}
-      <div className="content">
-        <h1>Название вашей фирмы</h1>
+        <div className="right-side">
+          <img src="./img/main-img-2.jpg" alt="right-side-img" />
+          <div className="language-switch" onClick={toggleLanguage}>
+            <div className={language === 'ru' ? 'active' : ''}>RU</div>
+            <div className={language === 'en' ? 'active' : ''}>EN</div>
+          </div>
+        </div>
+      </div>
+      <div className='about-us'>
+        <p className='about-us-text'>Создаем внутренние</p>
+        <p className='we-started-text'>МЫ НАЧИНАЛИ С МАЛЕНЬКОГО ОФИСА В БУДВЕ И КОМПЛЕКТАЦИИ НАБОРОМ ИТАЛЬЯНСКОЙ МЕБЕЛИ КВАРТИР ПО КАТАЛОГАМ</p>
+      </div>
+      <div className='aboust-us-second-part'>
+      <div className='about-us-text-container'>
+          <p><span className='about-us-heading'>О НАС</span></p>
+          <p><span className='large-digit-in-about-section'>15</span>лет опыта</p>
+          <p><span className='large-digit-in-about-section'>2</span>студии в Черногории и Москве</p>
+          <p><span className='large-digit-in-about-section'>200</span>фабрик, с которыми мы сотрудничаем</p>
+          <p><span className='large-digit-in-about-section'>300</span>дизайн-проектов</p>
+          <p><span className='large-digit-in-about-section'>14</span>человек в команде</p>
+        </div>
+
+        <div className="about-image-section">
+          <img src="./img/about-us-img.jpg" alt="about-us-img" />
+          <p>Мы перекрываем все вопросы, относящиеся к недвижимости</p>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Main;
+
+
+
+
+
+
